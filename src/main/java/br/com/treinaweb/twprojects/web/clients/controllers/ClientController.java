a1868b2fbaf76ed540da6d592e6e1426f0a9e2c6
lazarofonseca <lazaronobrega3@gmail.com>
2024-04-11 06:26:11 -0300

    Formatando os dados apresentados

--- src/main/java/br/com/treinaweb/twprojects/web/clients/controllers/ClientController.java
+++ src/main/java/br/com/treinaweb/twprojects/web/clients/controllers/ClientController.java
@@ -78,4 +78,7 @@ public class ClientController {
         return "redirect:/clients";
     }
 
+
+
+
 }
--- src/main/java/br/com/treinaweb/twprojects/web/home/controllers/HomeController.java
+++ src/main/java/br/com/treinaweb/twprojects/web/home/controllers/HomeController.java
@@ -3,6 +3,11 @@ package br.com.treinaweb.twprojects.web.home.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
@@ -12,4 +17,17 @@ public class HomeController {
     public String index() {
         return "home/index";
     }
+
+    @GetMapping("/format-data")
+    public ModelAndView formatData() {
+        var model = Map.of(
+                "date", LocalDate.of(2024, 4, 7),
+                "dateTime", LocalDateTime.of(2024, 4, 8, 14, 27),
+                "currency", 1024.51,
+                "percent", 2.0 / 100
+        );
+        return new ModelAndView("dados-formatados", model);
+    }
+
+
 }
